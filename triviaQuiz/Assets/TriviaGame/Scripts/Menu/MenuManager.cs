83c15e7bb08d721fc51d8de67184d89ec53492bf
michellefrd <michelleag99@gmail.com>
2024-04-19 12:51:14 +0200

    Added code for categories
    Created prefab for categories

---
+++ triviaQuiz/Assets/TriviaGame/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TigerForge.UniDB;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    private UniDB.Trivia triviaDB;
+    public Transform categoryButtonParent; // Parent object to hold instantiated buttons
+    public GameObject categoryButtonPrefab; // Prefab for category buttons
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triviaDB = new UniDB.Trivia();
+        OnLoadCategories();
+    }
+
+    private void OnLoadCategories()
+    {
+        var categories = triviaDB.GetTable_Categories();
+        _ = categories
+            .Select()
+            .Run(
+                (List<UniDB.Trivia.Categories.Record> data, Info info) =>
+                {
+                    if (info.isOK && data !=null)
+                    {
+                        foreach (var d in data)
+                        {
+                            GameObject newButton = Instantiate(categoryButtonPrefab, categoryButtonParent);
+                            newButton.name = "CategoryButton_" + d.name; // Naming the button for easier identification
+                            CategoryButton buttonScript = newButton.GetComponent<CategoryButton>();
+                            if (buttonScript != null)
+                            {
+                                buttonScript.categoryNameTxt.text = d.name; 
+                            }
+                            else
+                            {
+                                Debug.LogError("CategoryButton script not found on the instantiated prefab!");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("Error: " + info.error);
+                    }
+                }
+            );
+    }
+}
