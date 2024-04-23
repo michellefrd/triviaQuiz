91a2ac518940fdc2f8564fbc292b76dfe3aaf475
michellefrd <michelleag99@gmail.com>
2024-04-23 10:00:42 +0200

    - Added Helpers.cs
    - Commented LoginManager
    - Added LoadScene()

---
+++ triviaQuiz/Assets/TriviaGame/Scripts/Helpers.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Clase que contiene métodos que se utilizan múltiples veces
+/// </summary>
+public static class Helpers
+{
+    /// <summary>
+    /// Método utilizado para hashear la contraseña
+    /// </summary>
+    /// <param name="password">Contraseña a hashear</param>
+    public static string HashPassword(string password)
+    {
+        using (var sha256 = System.Security.Cryptography.SHA256.Create())
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(password);
+            var hash = sha256.ComputeHash(bytes);
+            return Convert.ToBase64String(hash); // Convertir el hash a string para almacenamiento
+        }
+    }
+
+   
+}
--- triviaQuiz/Assets/TriviaGame/Scripts/Login/LoginManager.cs
+++ triviaQuiz/Assets/TriviaGame/Scripts/Login/LoginManager.cs
@@ -1,4 +1,4 @@
-using System;
+
 using TigerForge.UniDB;
 using UnityEngine;
 using TMPro;
@@ -6,54 +6,86 @@ using ToastForUnity.Script.Core;
 using ToastForUnity.Script.Enum;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Este script controla la lógica de iniciar sesión en la aplicación
+/// </summary>
 public class LoginManager : MonoBehaviour
 {
-    [Header("Game objects")]
-    [SerializeField] private TMP_InputField emailInputField;
-    [SerializeField] private TMP_InputField passwordInputField;
-    [SerializeField] private Transform toastParent;
-    private UniDB.Trivia triviaDB;
-    
+    [Header("InputFields")]
+    // Campo de entrada para el correo electrónico
+    [SerializeField]
+    private TMP_InputField emailInputField;
 
-    // Color original del placeholder
+    // Campo de entrada para la contraseña
+    [SerializeField] private TMP_InputField passwordInputField;
+
+    [Header("Toast de información")]
+    // Objeto padre para los mensajes emergentes (toasts)
+    [SerializeField]
+    private Transform toastParent;
+
+    // Base de datos Trivia de UniDB (conexión)
+    private UniDB.Trivia triviaDB;
+
+    // Color original del texto del placeholder
     private Color originalPlaceholderColor = new Color(120f / 255f, 192f / 255f, 255f / 255f);
 
+    /// <summary>
+    /// Start solo se llama una vez durante la vida del script.
+    /// La diferencia entre Awake y Start es que Start solo se llama si la instancia del script está habilitada.
+    /// Esto le permite retrasar cualquier código de inicialización hasta que sea realmente necesario.
+    /// Awake siempre se llama antes de cualquier función de inicio.
+    /// Esto le permite ordenar la inicialización de scripts
+    /// </summary>
     void Start()
     {
+        // Inicializa la base de datos Trivia
         triviaDB = new UniDB.Trivia();
-        //Delete all playerPreferences
+        //Eliminar todas las preferencias del jugador guardadas en el dispositivo
         PlayerPrefs.DeleteAll();
+
         // Suscribir el método para restablecer el color de los campos al evento de cambio de texto
         emailInputField.onValueChanged.AddListener(delegate { ResetInputFieldColor(emailInputField); });
         passwordInputField.onValueChanged.AddListener(delegate { ResetInputFieldColor(passwordInputField); });
     }
-
+    
+    /// <summary>
+    /// Este método cambia el color de los componentes del inputField pasado
+    /// </summary>
+    /// <param name="inputField">Input field a cambiar</param>
     private void ResetInputFieldColor(TMP_InputField inputField)
     {
-        inputField.textComponent.color = Color.black; // Restablecer el color del texto
+        inputField.textComponent.color = Color.black; // Restablecer el color del texto del campo
         inputField.placeholder.color = originalPlaceholderColor; // Restablecer el color del placeholder
     }
 
+    /// <summary>
+    /// Este método es llamado desde el botón de inicio de sesión
+    /// Comprueba que los campos esten llenos y que el usuario y contraseña coincidan con los de la base de datos
+    /// </summary>
     public void OnClickLogin()
     {
+        // Verificación de que los campos no esten vacíos
         if (string.IsNullOrWhiteSpace(emailInputField.text) || string.IsNullOrWhiteSpace(passwordInputField.text))
         {
             if (string.IsNullOrWhiteSpace(emailInputField.text))
             {
-                
-                emailInputField.placeholder.color = Color.red;
+                // Cambiar color a rojo si el campo está vacío
+                emailInputField.placeholder.color = Color.red; 
             }
 
             if (string.IsNullOrWhiteSpace(passwordInputField.text))
             {
-                passwordInputField.placeholder.color = Color.red;
+                // Cambiar color a rojo si el campo está vacío
+                passwordInputField.placeholder.color = Color.red; 
             }
-
-            Toast.PopOut("Debes llenar todos los campos", ToastStatus.Danger,toastParent );
-            Debug.LogWarning("You must fill in all fields!");
+            // Mostrar mensaje de error
+            Toast.PopOut("Debes llenar todos los campos", ToastStatus.Danger, toastParent); 
+            Debug.LogWarning("Debes llenar todos los campos");
             return;
         }
 
+        //Instancia de la tabla usuarios
         var users = triviaDB.GetTable_Users();
         _ = users
             .SelectOne()
@@ -65,56 +97,56 @@ public class LoginManager : MonoBehaviour
                     {
                         if (info.hasData && d != null)
                         {
-                            var hashedInputPassword = HashPassword(passwordInputField.text);  // Hashear la contraseña ingresada
+                            // Hash de la contraseña ingresada
+                            var hashedInputPassword =
+                                Helpers.HashPassword(passwordInputField.text); 
                             if (d.password == hashedInputPassword)
                             {
-                                Debug.Log("Login successful: " + d.name + " " + d.last_name);
-                                PlayerPrefs.SetString("Email", d.email);
-                                PlayerPrefs.Save();
-                                SceneManager.LoadScene("MenuScene");
+                                Debug.Log("Login correcto: " + d.name + " " + d.last_name);
+                                // Guardar el correo en player prefs
+                                PlayerPrefs.SetString("Email", d.email); 
+                                // Guardar las preferencias
+                                PlayerPrefs.Save(); 
+                                // Cargar la siguiente escena (Menu)
+                                SceneManager.LoadScene("MenuScene"); 
                             }
                             else
                             {
-                                Toast.PopOut("Contraseña incorrecta", ToastStatus.Danger,toastParent );
-                                Debug.LogWarning("Incorrect password!");
+                                // Mostrar mensaje de contraseña incorrecta
+                                Toast.PopOut("Contraseña incorrecta", ToastStatus.Danger,
+                                    toastParent); 
+                                Debug.LogWarning("Contraseña incorrecta");
+                                // Cambiar color de texto a rojo
                                 passwordInputField.textComponent.color = Color.red;
-                                passwordInputField.placeholder.color = Color.red;
                             }
                         }
                         else
                         {
-                            Toast.PopOut("Usuario no encontrado", ToastStatus.Danger,toastParent );
-                            Debug.LogWarning("No user found with that email or user data is null!");
+                            // Mostrar mensaje de usuario no encontrado
+                            Toast.PopOut("Usuario no encontrado", ToastStatus.Danger,
+                                toastParent); 
+                            Debug.LogWarning("Usuario no encontrado");
                             emailInputField.textComponent.color = Color.red;
-                            emailInputField.placeholder.color = Color.red;
                         }
                     }
                     else
                     {
-                        Debug.LogError("Database error: " + info.error);
-                        Toast.PopOut("info.error", ToastStatus.Danger,toastParent );
+                        // Error de base de datos
+                        Debug.LogError("Error en la base de datos: " + info.error); 
+                        // Mostrar error de base de datos
+                        Toast.PopOut("info.error", ToastStatus.Danger, toastParent); 
                     }
                 }
             );
     }
 
-    public void OnClickSignUp()
+    /// <summary>
+    /// Este método se encarga de carga la escena del string que se pasa
+    /// </summary>
+    /// <param name="scene">Nombre de la escena a cargar</param>
+    public void LoadScene(string scene)
     {
-        SceneManager.LoadScene("SignUpScene");
+        SceneManager.LoadScene(scene);
     }
-    
-    public void OnClickRecoverPassword()
-        {
-            SceneManager.LoadScene("RecoverPasswordScene");
-        }
-    
-    private string HashPassword(string password)
-    {
-        using (var sha256 = System.Security.Cryptography.SHA256.Create())
-        {
-            var bytes = System.Text.Encoding.UTF8.GetBytes(password);
-            var hash = sha256.ComputeHash(bytes);
-            return Convert.ToBase64String(hash);  // Convertir el hash a string para almacenamiento
-        }
-    }
+
 }
