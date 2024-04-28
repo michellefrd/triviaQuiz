fef6fa2055068fe3d9b76fa01ce16b1f6d6dcd01
michellefrd <michelleag99@gmail.com>
2024-04-28 11:29:33 +0200

    Fixed issues with LeaderboardItem and RecoverPasswordScene

--- triviaQuiz/Assets/TriviaGame/Scripts/Leaderboard/LeaderboardItem.cs
+++ triviaQuiz/Assets/TriviaGame/Scripts/Leaderboard/LeaderboardItem.cs
@@ -2,7 +2,7 @@ using TMPro;
 using UnityEngine;
 
 
-public class LeaderboardItem : MonoBehaviour
+public class LeaderboardItem: MonoBehaviour
 {
     public TMP_Text name;
     public TMP_Text points;
